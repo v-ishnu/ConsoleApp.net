3f83934c78aa406f15fed28014697d326180951d
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2024-11-28 02:58:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+    // Basically namespace is a container, where we can execute our code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo World");
+            Console.ReadLine();
+            Console.Write("I Love");
+        }
+    }
+}
