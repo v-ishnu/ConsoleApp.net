203b602ee246cc3109bffae7bad283c85c48e236
Vishnu Prakash <65424078+v-ishnu@users.noreply.github.com>
2024-11-30 01:21:51 +0530

    Understand Concept

--- Program.cs
+++ Program.cs
@@ -49,17 +49,61 @@ namespace ConsoleApp
              */
 
             // Example of Explicit Casting
-            //int x1 = (int)3.5;
-            //Console.WriteLine(x1);
+            // int x1 = (int)3.5;
+            // Console.WriteLine(x1);
 
-            //Example of Implicit Casting
-            int x2 = 5;
-            float y = x2;
-            double x3 = y;
-            Console.WriteLine(x3);
+            // Example of Implicit Casting
+            // int x2 = 5;
+            // float y = x2;
+            // double x3 = y;
+            // Console.WriteLine(x3);
+
+
+            //Console.WriteLine("Enter your name:");
+            //String name = Console.ReadLine();
+            //Console.WriteLine("Hey " + name);
+
+            //Console.WriteLine("How many candies u want:");
+            //String c = Console.ReadLine();
+            //Console.WriteLine("You will get 4 more candies:" +
+            //    (Convert.ToInt32(c) + 4));
+
+
+            /* Opertaor in C# 
+             * 1.) Airthmetic Operator
+             * 2.) Assignment Operator
+             * 3.) Logical Operator
+             * 4.) Comparison Operator
+             */
+
+
+            //int a = 4;
+            //int b = 5;
+            //Console.WriteLine("Sum of A + B = " + (a + b));
+            //Console.WriteLine("Sum of A - B = " + (a - b));
+            //Console.WriteLine("Sum of A * B = " + (a * b));
+            //Console.WriteLine("Sum of A / B = " + (a / b));
+
+            //Assignment Operator 
+            //b += 6;
+
+            //int a1 = Math.Min(a, b);
+            //Double a2 = Math.Sqrt(49);
+            //Console.WriteLine(a2);
+
+
+            //string hello = "Namshkar!";
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello + "I'm self Vishnu Prakash");
+
+            //string name = Console.ReadLine();
+            //string candies = Console.ReadLine();
+            //Console.WriteLine($"Your Name is {name}. You will get {candies}");
+
+            var c1 = "Vishnu";
+            Console.WriteLine(c1.GetType());
+
             Console.ReadLine();
-
-
         }
     }
 }
